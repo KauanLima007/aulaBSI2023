5b64469de582b104d993f1f32250ac0ab5293e66
KauanLima007 <143764709+KauanLima007@users.noreply.github.com>
2023-09-23 12:58:15 -0300

    Delete aulaBSI2023-main directory

--- aulaBSI2023-main/APIExemplo/src/main/java/org/example/repository/alunos.java
+++
@@ -1,9 +0,0 @@
-package org.example.repository;
-
-
-import org.example.entity.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface alunos extends JpaRepository<Aluno, Integer> {
-
-}
